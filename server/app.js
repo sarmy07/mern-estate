13180385f15c82ea70224427001e9ff5e129442e
samuel <ks.orji@gmail.com>
2025-01-16 08:33:10 +0100

    connected to database

--- server/app.js
+++ server/app.js
@@ -1,9 +1,23 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
+const mongoose = require("mongoose");
 
 const port = process.env.PORT || 3000;
+const mongo_url = `${process.env.mongo_uri}`;
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+
+const connect = async () => {
+  try {
+    await mongoose.connect(mongo_url);
+    console.log("connected to mongodb");
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  } catch (error) {
+    console.log("could not connect to mongodb because...");
+    console.log(error);
+  }
+};
+
+connect();
